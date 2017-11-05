af433bd7963152479560f4ec5241dc48b313c47d
Wellington Moreno <jwellington.moreno@gmail.com>
2017-11-05 11:40:12 -0800

    Adds initial commit

---
+++ AlchemyTest/AlchemyTest/AlchemyTest.h
@@ -0,0 +1,19 @@
+//
+//  AlchemyTest.h
+//  AlchemyTest
+//
+//  Created by Wellington Moreno on 11/5/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlchemyTest.
+FOUNDATION_EXPORT double AlchemyTestVersionNumber;
+
+//! Project version string for AlchemyTest.
+FOUNDATION_EXPORT const unsigned char AlchemyTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemyTest/PublicHeader.h>
+
+
