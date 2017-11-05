f866ab99e635cde48e842c65a36b36eaa39a0495
Wellington Moreno <jwellington.moreno@gmail.com>
2017-11-05 15:03:17 -0800

    Adds AlchemyTest Swift file

---
+++ AlchemyTest/AlchemyTest/AlchemyTest.swift
@@ -0,0 +1,38 @@
+//
+//  AlchemyTest.swift
+//  AlchemyTest
+//
+//  Created by Wellington Moreno on 11/5/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+
+class AlchemyTest
+{
+    
+}
+
+func assertThat(_ expression: Bool)
+{
+    if !expression
+    {
+        
+    }
+}
+
+func assertFalse(_ expression: Bool)
+{
+    if expression
+    {
+        
+    }
+}
+
+func assertTrue(_ expression: Bool)
+{
+    if !expression
+    {
+        
+    }
+}
