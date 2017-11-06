42b0d06651673ebd407138ad9d448e09a8f3bf62
Wellington Moreno <jwellington.moreno@gmail.com>
2017-11-05 22:52:03 -0800

    Adds comment test functions

--- AlchemyTest/AlchemyTest/AlchemyTest.swift
+++ AlchemyTest/AlchemyTest/AlchemyTest.swift
@@ -7,32 +7,73 @@
 //
 
 import Foundation
+import XCTest
 
 class AlchemyTest
 {
     
 }
 
-func assertThat(_ expression: Bool)
+internal func fail(message: String = "Assertion failed")
+{
+    XCTFail(message)
+}
+
+public func assertThat(_ expression: Bool)
 {
     if !expression
     {
-        
+        fail()
     }
 }
 
-func assertFalse(_ expression: Bool)
+public func assertFalse(_ expression: Bool)
 {
     if expression
     {
-        
+        fail()
     }
 }
 
-func assertTrue(_ expression: Bool)
+public func assertTrue(_ expression: Bool)
 {
     if !expression
     {
-        
+        fail()
+    }
+}
+
+public func assertNotNil(variable: Any?)
+{
+    if variable == nil
+    {
+        fail(message: "Expected non-nil value")
+    }
+}
+
+public func assertNil(variable: Any?)
+{
+    if let variable = variable
+    {
+        fail(message: "Expected nil value, but [\(variable)]")
+    }
+}
+
+public func assertEquals<T: Equatable>(first: T?, second: T?)
+{
+    if first == nil && second == nil
+    {
+        return
+    }
+    
+    guard let _first = first, let _second = second else
+    {
+        fail(message: "Not equal: [\(first)] & [\(second)]")
+        return
+    }
+    
+    if _first != _second
+    {
+        fail(message: "Not equal: [\(_first)] & [\(_second)]")
     }
 }
