bf58bc414d89b24cec9e7fd545ec110343b655de
Wellington Moreno <wellington@censio.love>
2017-12-29 15:43:39 -0800

    Adds filename and line number to assert declarations

--- AlchemyTest/AlchemyTest/AlchemyTest.swift
+++ AlchemyTest/AlchemyTest/AlchemyTest.swift
@@ -14,52 +14,52 @@ class AlchemyTest
     
 }
 
-public func failTest(_ message: String = "Assertion failed", _ functionName: String = #function, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func failTest(_ message: String = "Assertion failed", functionName: String = #function, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     XCTFail(functionName + " | " + message, file: filename, line: lineNumber)
 }
 
-public func assertThat(_ expression: Bool, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertThat(_ expression: Bool, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if !expression
     {
-        failTest()
+        failTest(filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertFalse(_ expression: Bool, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertFalse(_ expression: Bool, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if expression
     {
-        failTest()
+        failTest(filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertTrue(_ expression: Bool, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertTrue(_ expression: Bool, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if !expression
     {
-        failTest()
+        failTest(filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertNotNil(_ variable: Any?, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNotNil(_ variable: Any?, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if variable == nil
     {
-        failTest("Expected non-nil value")
+        failTest("Expected non-nil value", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertNil(_ variable: Any?, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNil(_ variable: Any?, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if let variable = variable
     {
-        failTest("Expected nil value, but [\(variable)]")
+        failTest("Expected nil value, but [\(variable)]", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertEquals<T: Equatable>(_ first: T?, _ second: T?, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertEquals<T: Equatable>(_ first: T?, _ second: T?, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if first == nil && second == nil
     {
@@ -68,17 +68,17 @@ public func assertEquals<T: Equatable>(_ first: T?, _ second: T?, _ filename: St
     
     guard let _first = first, let _second = second else
     {
-        failTest("Not equal: [\(first)] & [\(second)]")
+        failTest("Not equal: [\(first)] & [\(second)]", filename: filename, lineNumber: lineNumber)
         return
     }
     
     if _first != _second
     {
-        failTest("Not equal: [\(_first)] & [\(_second)]")
+        failTest("Not equal: [\(_first)] & [\(_second)]", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertEquals<T: Equatable>(_ first: [T], _ second: [T], _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertEquals<T: Equatable>(_ first: [T], _ second: [T], filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if first.isEmpty && second.isEmpty
     {
@@ -87,7 +87,7 @@ public func assertEquals<T: Equatable>(_ first: [T], _ second: [T], _ filename: 
     
     guard first.count == second.count else
     {
-        failTest("Arrays have different lengths")
+        failTest("Arrays have different lengths", filename: filename, lineNumber: lineNumber)
         return
     }
     
@@ -97,38 +97,38 @@ public func assertEquals<T: Equatable>(_ first: [T], _ second: [T], _ filename: 
         
         guard secondValue == firstValue else
         {
-            failTest("Arrays are different [@\(index)] | \(secondValue) != \(firstValue)")
+            failTest("Arrays are different [@\(index)] | \(secondValue) != \(firstValue)", filename: filename, lineNumber: lineNumber)
             return
         }
     }
 }
 
-public func assertEquals(_ first: Double, _ second: Double, withMarginOfError marginOfError: Double, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertEquals(_ first: Double, _ second: Double, withMarginOfError marginOfError: Double, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     let left = first - marginOfError
     let right = first + marginOfError
     
     guard left <= second else
     {
-        failTest("\(first) != \(second) within ε (\(marginOfError))")
+        failTest("\(first) != \(second) within ε (\(marginOfError))", filename: filename, lineNumber: lineNumber)
         return
     }
     
     guard second <= right else
     {
-        failTest("\((first)) != \(second) within ε (\(marginOfError))")
+        failTest("\((first)) != \(second) within ε (\(marginOfError))", filename: filename, lineNumber: lineNumber)
         return
     }
 }
 
-public func assertNotEquals<T: Equatable>(_ first: T?, _ second: T?, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNotEquals<T: Equatable>(_ first: T?, _ second: T?, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     
     guard let _first = first, let _second = second else
     {
         if first == second
         {
-            failTest("Expected different values, but [\(first) & \(second)] are the same.")
+            failTest("Expected different values, but [\(first) & \(second)] are the same.", filename: filename, lineNumber: lineNumber)
         }
         
         return
@@ -136,47 +136,47 @@ public func assertNotEquals<T: Equatable>(_ first: T?, _ second: T?, _ filename:
     
     if _first == _second
     {
-        failTest("Expected different values, but [\(_first) & \(_second)]")
+        failTest("Expected different values, but [\(_first) & \(_second)]", filename: filename, lineNumber: lineNumber)
     }
 }
 
 
-public func assertNotEmpty(_ string: String, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNotEmpty(_ string: String, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if string.isEmpty
     {
-        failTest("String is empty")
+        failTest("String is empty", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertNotEmpty(_ collection: AnyCollection<Any>, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNotEmpty(_ collection: AnyCollection<Any>, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if collection.isEmpty
     {
-        failTest("Collection is empty")
+        failTest("Collection is empty", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertNotEmpty(_ array: [Any], _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertNotEmpty(_ array: [Any], filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if array.isEmpty
     {
-        failTest("Array is empty")
+        failTest("Array is empty", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertEmpty(_ collection: AnyCollection<Any>, _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertEmpty(_ collection: AnyCollection<Any>, filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if !collection.isEmpty
     {
-        failTest("Collection is not empty. Has \(collection.count) elements")
+        failTest("Collection is not empty. Has \(collection.count) elements", filename: filename, lineNumber: lineNumber)
     }
 }
 
-public func assertEmpty(_ array: [Any], _ filename: StaticString = #file, _ lineNumber: UInt = #line)
+public func assertEmpty(_ array: [Any], filename: StaticString = #file, lineNumber: UInt = #line)
 {
     if !array.isEmpty
     {
-        failTest("Array is not empty. Has \(array.count) elements")
+        failTest("Array is not empty. Has \(array.count) elements", filename: filename, lineNumber: lineNumber)
     }
 }
