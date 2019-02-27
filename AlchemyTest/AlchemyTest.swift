48a1c8b812d2965a45ca62274739cf954a6c0dff
Wellington Moreno <wellington@censio.love>
2019-02-27 00:35:59 -0800

    Moves project up to root folder

---
+++ AlchemyTest/AlchemyTest.swift
@@ -0,0 +1,127 @@
+//
+//  AlchemyTest.swift
+//  AlchemyTest
+//
+//  Created by Wellington Moreno on 03/02/2018.
+//  Copyright © 2018 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+
+//======================================
+// MARK: ALCHEMY TEST
+//======================================
+protocol AlchemyTestProtocol
+{
+    static func beforeTests()
+
+    static func afterTests()
+
+    func beforeEachTest()
+
+    func afterEachTest()
+}
+
+open class AlchemyTest: XCTestCase
+{
+    /// The number of times a test is repeated whenever `repeatTest()` feature is used
+    open var iterations: Int { return 100 }
+
+    /// The default async timeout used if one isn't specified explicitly when using `asyncTest()`
+    open var defaultAsyncTimeout: TimeInterval { return 15.0 }
+    
+    public typealias Block = () -> Void
+
+    open override static func setUp()
+    {
+        super.setUp()
+        beforeTests()
+    }
+
+    open override static func tearDown()
+    {
+        super.tearDown()
+        afterTests()
+    }
+
+    open override func setUp()
+    {
+        super.setUp()
+        beforeEachTest()
+    }
+
+    open override func tearDown()
+    {
+        super.tearDown()
+        afterEachTest()
+    }
+
+    open class func beforeTests()
+    {
+    }
+
+    open class func afterTests()
+    {
+    }
+
+    open func beforeEachTest()
+    {
+    }
+
+    open func afterEachTest()
+    {
+    }
+    
+    /**
+        Repeats a block of test code `iterations` times.
+        > Note that the test is reset on each iteration, like so:
+     
+        1. `beforeEachTest()`
+        2. `block()`
+        3. `afterEachTest()`
+     
+        - Parameter block: The test block to repeat and execute
+     */
+    public func repeatTest(_ block: Block)
+    {
+        repeatTest(iterations, block)
+    }
+
+    public func repeatTest(_ iterations: Int, _ block: Block)
+    {
+        (0..<iterations).forEach
+        { _ in
+            beforeEachTest()
+            block()
+            afterEachTest()
+        }
+    }
+    
+    @available(*, deprecated, message: "Use `repeatTest` instead")
+    public func runTest(_ block: Block)
+    {
+        repeatTest(iterations, block)
+    }
+    
+    @available(*, deprecated, message: "Use `repeatTest` instead")
+    public func runTest(iterations: Int, _ block: Block)
+    {
+        repeatTest(iterations, block)
+    }
+
+    public func asyncTest(_ callback: (XCTestExpectation) -> Void)
+    {
+        asyncTest(defaultAsyncTimeout, callback)
+    }
+
+    public func asyncTest(_ timeout: TimeInterval, _ callback: (XCTestExpectation) -> Void)
+    {
+        let promise = expectation(description: "operation")
+        callback(promise)
+        wait(for: [promise], timeout: timeout)
+    }
+
+
+}
---
+++ AlchemyTestTests/AlchemyTestPlusAssertionsTest.swift
@@ -0,0 +1,63 @@
+//
+//  AlchemyTestPlusAssertionsTest.swift
+//  AlchemyTest
+//
+//  Created by Wellington Moreno on 11/5/17.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+@testable import AlchemyTest
+import XCTest
+
+class AlchemyTestPlusAssertionsTest: XCTestCase
+{
+
+    private var testIterations = 100
+
+    override func setUp()
+    {
+        super.setUp()
+    }
+
+    override func tearDown()
+    {
+        super.tearDown()
+    }
+
+    func testDoubleAssertEqualsWithMarginOfError()
+    {
+        assertEquals(0.5, 0.6, withMarginOfError: 0.15)
+        assertEquals(0.51, 0.65, withMarginOfError: 0.2)
+        assertEquals(12.3, 12.35, withMarginOfError: 0.1)
+        assertEquals(543.21, 541.01, withMarginOfError: 4.0)
+    }
+
+    func testDateAssertEqualsWithMarginOfError()
+    {
+        (1...testIterations).forEach
+        { i in
+
+            let firstDate = Date()
+            let secondDate = firstDate.addingTimeInterval(1.0)
+            assertEquals(firstDate, secondDate, withMarginOfError: 2.0)
+        }
+    }
+
+    func testCGFloatAssertEqualsWithMarginOfError()
+    {
+        assertEquals(CGFloat(0.5), CGFloat(0.6), withMarginOfError: CGFloat(0.15))
+        assertEquals(CGFloat(0.51), CGFloat(0.65), withMarginOfError: CGFloat(0.2))
+        assertEquals(CGFloat(12.3), CGFloat(12.35), withMarginOfError: CGFloat(0.1))
+        assertEquals(CGFloat(543.21), CGFloat(541.01), withMarginOfError: CGFloat(4.0))
+    }
+
+    func testDecimalAssertEqualsWithMarginOfError()
+    {
+        assertEquals(Decimal(0.5), Decimal(0.6), withMarginOfError: Decimal(0.15))
+        assertEquals(Decimal(0.51), Decimal(0.65), withMarginOfError: Decimal(0.2))
+        assertEquals(Decimal(12.3), Decimal(12.35), withMarginOfError: Decimal(0.1))
+        assertEquals(Decimal(543.21), Decimal(541.01), withMarginOfError: Decimal(4.0))
+    }
+
+
+}
